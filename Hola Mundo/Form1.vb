9c0d4e6ed2e3f1deaf7659542882cc8cb89e5b60
Israel <israelyataco@yahoo.com>
2017-05-20 16:45:32 -0400

    Iniciamos

---
+++ Hola Mundo/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        MsgBox("Hola Mundo y Sus Alrededores", MsgBoxStyle.Information, "Ventanilla Ok")
+
+    End Sub
+End Class
